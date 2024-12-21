5de2b3db349231544e93f34cea6a300e8bea733b
znyisztor77 <znyisztor77@gmail.com>
2024-12-21 21:09:12 +0100

    Stopper hozzáadva . még finomításra vár!

--- horgaszcucc.ino
+++ horgaszcucc.ino
@@ -34,8 +34,8 @@ char daysOfTheWeek[7][12] = {"Vasarnap", "Hetfo", "Kedd", "Szerda", "Csutortok",
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-const int HX711_dout = 16; //mcu > HX711 dout pin, ESP32 GPIO16
-const int HX711_sck = 4; //mcu > HX711 sck pin, ESP32 GPIO4
+const int HX711_dout = 25; //mcu > HX711 dout pin, ESP32 GPIO16
+const int HX711_sck = 26; //mcu > HX711 sck pin, ESP32 GPIO4
 
 //HX711 constructor:
 HX711_ADC LoadCell(HX711_dout, HX711_sck);
@@ -44,8 +44,10 @@ const int calVal_eepromAdress = 0;
 unsigned long t = 0;
 volatile boolean newDataReady;
 
+const int tareButton = 2;
 int readingWeight;
 int lastReading;
+int maxWeight=0;
 
 //FishCount button debounce
 const int fishCountButtonPin = 15;
@@ -55,37 +57,29 @@ int fishCountlastButtonState = HIGH;
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
-/*void fishCountButton(int count){
-  
-  int reading2 = digitalRead(fishCountButtonPin);
-  if (reading2 != fishCountlastButtonState) {
-    lastDebounceTime = millis();
-  }
-
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (reading2 != fishCountButtonState) {
-      fishCountButtonState = reading2;
-
-      if (fishCountButtonState == LOW) {
-        count = count + 1;
-      }
-    }
-  }
-  
-  fishCountlastButtonState = reading2;
-}*/
+//Stopwatch
+int hours = 0;
+int minutes = 0;
+long int seconds = 0;
+int hoursStop;
+int minutesStop;
+long int secondsStop;
+unsigned long previousMillis = 0;
+const long  interval = 1000;
+int weightState = 60;
+bool measureState = true;
 
 void displayWeight(int weight){
-  display.println();
-  display.println("Suly meres:");
-  display.setTextSize(2);  
-  display.println(String(weight) + "g");
+  //display.println();
+  display.println("Suly meres: "+ String(weight) + "g" );
+  //display.setTextSize(2);  
+  //display.println(String(weight) + "g");
   
 }
 
 void displayFishCounter(int fishcount){
   //display.println();  
-  display.setTextSize(2);  
+  display.setTextSize(1);  
   display.println(String(fishcount)+ " db");
   
 }
@@ -99,17 +93,49 @@ void displayTimeTemp(){
   display.println((String(temp)+" "+char(247)+"C"));
   
 }
+
+void displayMax(int weight){
+  if(maxWeight<weight){
+    maxWeight = weight;
+    }
+   display.setTextSize(1);  
+   display.println("Max nyomas: " + String(maxWeight));
+  }
+
+void displayStopWatch(int h, int m, long int s){
+      if(hours<10){
+        display.print("0"+String(hours)+ ":");
+      }
+      
+      else{ display.print(String(hours)+ ":");
+      }
+      if(minutes<10){
+        display.print("0"+String(minutes)+ ":");
+      }
+      
+      else{ display.print(String(minutes)+ ":");
+      }
+      if(seconds<10){
+        display.print("0"+String(seconds));
+      }
+      
+      else{ display.print(String(seconds));
+      }
+}
+
   
 void setup() {
   Serial.begin(115200); delay(10);
   Serial.println();
   Serial.println("Starting...");  
 
-  pinMode(2, INPUT_PULLUP);
-  pinMode(15, INPUT_PULLUP);
+  pinMode(tareButton, INPUT_PULLUP);
+  pinMode(fishCountButtonPin, INPUT_PULLUP);
   
   float calibrationValue; // calibration value
-  calibrationValue = 32.47; // uncomment this if you want to set this value in the sketch
+  calibrationValue = 32.47; // uncomment this if you want to set this value in the sketch 1 mérő cellával
+  //calibrationValue = -2.77; // uncomment this if you want to set this value in the sketch 2 mérő cellával
+
   
   #if defined(ESP8266) || defined(ESP32)
   //EEPROM.begin(512); // uncomment this if you use ESP8266 and want to fetch the value from eeprom
@@ -151,14 +177,14 @@ void dataReadyISR() {
     }
 }
   
-void loop() {
-  
+void loop(){  
   const int serialPrintInterval = 0; //increase value to slow down serial print activity
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(1);
   display.setCursor(0, 0);
   displayTimeTemp();
+//----------------------------------------------------------------------
   // get smoothed value from the dataset:
   if (newDataReady) {
     if (millis() > t + serialPrintInterval) {
@@ -175,13 +201,13 @@ void loop() {
     }
   }
   displayWeight(readingWeight);
-     
+//------------------------------------------------------------------------
   // Tare button with debounce GPIO2
-  boolean newState = digitalRead(2); // Get current button state.
+  boolean newState = digitalRead(tareButton); // Get current button state.
   boolean oldState = HIGH;
   if((newState == LOW) && (oldState == HIGH)) { // Check if state changed from high to low (button press).
     delay(5);// Short delay to debounce button.
-    newState = digitalRead(2);// Check if button is still low after debounce.
+    newState = digitalRead(tareButton);// Check if button is still low after debounce.
     if(newState == LOW) {      // Yes, still low
         LoadCell.tareNoDelay();
         if(count != 0){
@@ -189,22 +215,12 @@ void loop() {
           }
      }
    }
-  /* 
-  // Fishcount button GPIO15 
-  boolean newState2 = digitalRead(15); // Get current button state.
-  boolean oldState2 = HIGH;
-  if((newState2 == LOW) && (oldState2 == HIGH)) { // Check if state changed from high to low (button press).
-    delay(50);// Short delay to debounce button.
-    newState2 = digitalRead(15);// Check if button is still low after debounce.
-    if(newState2 == LOW) {      // Yes, still low
-        count = count + 1;
-     }
-   }*/
+
+//Fish counter debounce-------------------------------------------------------- 
   int readingButtonState = digitalRead(fishCountButtonPin);
   if (readingButtonState != fishCountlastButtonState) {
     lastDebounceTime = millis();
   }
-
   if ((millis() - lastDebounceTime) > debounceDelay) {
     if (readingButtonState != fishCountButtonState) {
       fishCountButtonState = readingButtonState;
@@ -214,12 +230,80 @@ void loop() {
       }
     }
   }
-  
   fishCountlastButtonState = readingButtonState;
-
-  //fishCountButton(count);
   displayFishCounter(count);
+//------------------------------------------------------------------------------
+  
+  displayMax(readingWeight);
+//------------------------------------------------------------------------------
+//Stopwatch
+  unsigned long currentMillis = millis();
+  
+  if(weightState < readingWeight){
+    measureState = false;
     
+  }
+  else{
+    measureState = true;
+  }
+    
+  if(measureState == false){
+    if(currentMillis - previousMillis >= interval){
+      previousMillis = currentMillis;
+      seconds++;
+      
+      if (seconds>59){ 
+      seconds=0;   
+      minutes++; 
+      } 
+      if (minutes>59){
+      hours++; 
+      minutes=0; 
+      } 
+      if(hours>23){ 
+      hours=0; 
+      }   
+    }
+  }
+  
+  displayStopWatch(hours, minutes, seconds);
+  if(measureState==false){
+   display.println();
+  }
+  if (measureState == true)
+  {
+    display.println();
+    display.println("Stopped...");
+    hoursStop = hours;
+    minutesStop = minutes;
+    secondsStop = seconds;    
+    delay(200);
+    hours=0;
+    minutes=0;
+    seconds=0;
+  }
+ 
+ //displayStopWatch(hoursStop, minutesStop, secondsStop );  
+ if(hoursStop<10){
+        display.print("0"+String(hoursStop)+ ":");
+      }
+      
+      else{ display.print(String(hoursStop)+ ":");
+      }
+      if(minutes<10){
+        display.print("0"+String(minutesStop)+ ":");
+      }
+      
+      else{ display.print(String(minutesStop)+ ":");
+      }
+      if(secondsStop<10){
+        display.print("0"+String(secondsStop));
+      }
+      
+      else{ display.print(String(secondsStop));
+      }
+//-----------------------------------------------------------------------
+  
   // receive command from serial terminal, send 't' to initiate tare operation: (a terminálra a t küldése után újra tárázik) 
   if (Serial.available() > 0) {
     char inByte = Serial.read();
